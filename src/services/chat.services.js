51515847a7c7fda96cbeffc726d4265519477d70
Youva-hch <y_houche@hetic.eu>
2025-03-14 14:10:11 +0100

    fin du socket
    j'ai finis d'ajouter les sockets au code

--- src/services/chat.services.js
+++ src/services/chat.services.js
@@ -26,4 +26,30 @@ io.on('connection', (socket) => {
     console.log('un utlisateur est connecter');
     socket.on('chatMessage', (msg) => {
         const date = new Date();
+        //creation d'un nouveau message
+        const newMessage = new Message({
+            id: String(Date.now()),  
+            name: msg.name,         
+            message: msg.message,    
+            date: date.toLocaleDateString(),  
+            heure: date.toLocaleTimeString(), 
+          });
+      
+          // Sauvegarde le message dans MongoDB
+          newMessage.save()
+            .then(() => {
+              // permet a tout les client de recevoir le message en temps reel
+              io.emit('chatMessage', msg);
+            })
+            .catch((err) => console.log(err));  // au cas ou une erreur surveitn durant l'envoie d'un message
+        });
+
+        socket.on('disconnect', () => {
+            console.log("Un utilisateur s'est déconnecté");
+        });
 });
+
+server.listen(3000, () => {
+    console.log('Serveur démarré sur http://localhost:3000'); 
+  });
+
