5402e899a8d7ec30a2a20fcb7eb6817d424a60d7
Clara Baptista <baptista.clara@hotmail.com>
2025-03-14 15:34:39 +0100

    Ajout de la fonction d'écrire de message

--- src/services/chat.services.js
+++ src/services/chat.services.js
@@ -1,20 +1,32 @@
-import Message from '../models/chat.model.js';
+import Chat from '../models/chat.model.js';
 
-// Fonction pour récupérer tous les messages
-export const getMessages = async (req, res) => {
+export const createMessage = async (req, res) => {
+  const { id, name, message, date, heure } = req.body;
   try {
-    const logs = await Message.find();
+    const newMessage = new Chat({ id, name, message, date, heure });
+    
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (err) {
+    res.status(500).json({ message: 'Erreur lors de la création du message', error: err });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const logs = await Chat.find();
     res.json(logs);
   } catch (err) {
     res.status(500).json({ message: 'Erreur lors de la récupération des messages', error: err });
   }
 };
 
-// Fonction pour supprimer un message par ID
+
 export const deleteMessage = async (req, res) => {
   const { id } = req.params;
   try {
-    await Message.deleteOne({ id });
+    await Chat.deleteOne({ id });
     res.json({ message: 'Message supprimé' });
   } catch (err) {
     res.status(500).json({ message: 'Erreur lors de la suppression du message', error: err });
